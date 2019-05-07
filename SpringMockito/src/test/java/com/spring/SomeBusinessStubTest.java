a2608ee123275de551bfe6091d74fe823c8391ca
JeanPascal <JeanPascal@9.145.55.44>
2019-05-07 15:32:06 +0300

    mockito mocking annotations

--- SpringMockito/src/test/java/com/spring/SomeBusinessStubTest.java
+++ SpringMockito/src/test/java/com/spring/SomeBusinessStubTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class SomeBusinessTest {
+public class SomeBusinessStubTest {
 	
 	@Test
 	public void findTheGreatestFromAllData() {
