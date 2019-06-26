3970394e32079ce63d6d4c9bbf648bee5a101d94
JeanPascal <JeanPascal@9.145.90.194>
2019-06-26 15:22:54 +0300

    created mvc configuration and added web dependency

---
+++ SpringSecurity/src/main/java/com/spring/MvcConfig.java
@@ -0,0 +1,15 @@
+package com.spring;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer{
+	public void addViewController(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+		
+	}
+
+}
--- SpringSecurity/src/main/java/com/spring/WebSecurityConfig.java
+++ SpringSecurity/src/main/java/com/spring/WebSecurityConfig.java
@@ -29,6 +29,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		.and()
 		.logout()
 		.permitAll();
+		
+		
 	}
 	
 	@Bean
