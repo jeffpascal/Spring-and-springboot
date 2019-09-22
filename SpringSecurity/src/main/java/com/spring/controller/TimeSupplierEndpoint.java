91c55b40182a976491edc26951e19438e3cf7d8c
Pascal Jean-Francois <jeffpascal96@gmail.com>
2019-09-22 22:30:41 +0300

    Update TimeSupplierEndpoint.java

--- SpringSecurity/src/main/java/com/spring/controller/TimeSupplierEndpoint.java
+++ SpringSecurity/src/main/java/com/spring/controller/TimeSupplierEndpoint.java
@@ -13,18 +13,13 @@ public class TimeSupplierEndpoint {
 		return LocalDate.MAX;
 	}
 	
-	@GetMapping(path = "/userTime", produces = "application/json")
+	@GetMapping(path = "/time", produces = "application/json")
 	public LocalDate lastTime() {
 		return LocalDate.MIN;
 	}
 	
 	@GetMapping(path = "/secretTime", produces = "application/json")
 	public LocalDate secretCurrentTime() {
-		return LocalDate.now();
-	}
-
-	@GetMapping(path = "/time", produces = "application/json")
-	public LocalDate currentTimes() {
 		return LocalDate.now();
 	}
 }
